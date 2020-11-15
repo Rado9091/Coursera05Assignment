08d209c7910ce1475d68f465e6771ffc6cfaf901
Rado9091 <71198550+Rado9091@users.noreply.github.com>
2020-11-15 14:16:51 +0100

    Update app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,14 +1,38 @@
-(function (){
-  'use strict';
-  //create Module
-  angular.module('LunchCheck',[])
-  //create controller
-  .controller('LunchCheckController', function($scope){
-    var amountOrder = 0;
-    $scope.amount = " ";
-    $scope.checkAmount = function(){
-      return "It is too Much!";
-    };
+(function () {
+'use strict';
+//Create Module
+angular.module('LunchCheck', [])
+//Create Controller
+.controller('LunchCheckController', LunchCheckController);
 
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  //Initiate the variables
+  $scope.input=" ";
+  $scope.message=" ";
+  
+  //Create the function which checks the amount too much or not
+  $scope.CheckIfTooMuch = function () {
+  //Take the value from the user
+  var input=$scope.input;
+  //Split the value entered in the input file when comma(,) is found
+  var OrderedItems =input.split(',');
+
+ //Creating for Loop each item in the list
+  OrderedItems.forEach((item, i) => {
+    console.log(i);
+    if (input ==" ") {
+        console.log('Please insert data!');
+        $scope.message="Please insert data!";
+    }
+    else if (i<2) {
+      console.log('Enjoy!');
+        $scope.message="Enjoy!";
+    }else if (i>2) {
+      console.log('Too Much!');
+      $scope.message="Too Much!";
+    }
   });
+  };
+}
 })();
